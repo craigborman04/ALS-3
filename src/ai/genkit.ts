cc11f3f673e0ad5203aea0646b9aa1a0e9e771f8
Craig Borman <Craig.Borman@gmail.com>
2025-07-29 10:36:03 +0000

    Please connect to my shizentai-aikikai project so i can use my gemini pr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
+  plugins: [googleAI({projectId: 'shizentai-aikikai'})],
+  model: 'googleai/gemini-pro',
 });
